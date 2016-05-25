8c4f26bcfc3334419153a8386d94f8fd2e8efdbc
khun <rigel517@gmail.com>
2016-05-25 15:36:54 +0900

    create index and make migration

--- e10/urls.py
+++ e10/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'blog.views.index'),
+    url(r'^$', views.index),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
